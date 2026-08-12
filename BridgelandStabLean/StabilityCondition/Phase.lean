4d46ea6fb9f377a17f66adc2d1bde9f9b926bce1
Chris Dare <chris.dare@nalej.com>
2026-08-12 18:43:49 -0400

    feat: migrate slicing orders and Bayer bounds (#153)

--- BridgelandStabLean/StabilityCondition/Phase.lean
+++ BridgelandStabLean/StabilityCondition/Phase.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
 import BridgelandStabLean.StabilityCondition.Phase.UniformContinuity
+import BridgelandStabLean.StabilityCondition.Phase.Order
 
 /-!
 # Phase transformations
