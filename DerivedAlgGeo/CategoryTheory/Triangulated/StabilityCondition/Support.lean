87d5f8adfe703dd35cd896775b0956fd60b0a420
Chris Dare <chris.dare@nalej.com>
2026-08-15 01:11:21 -0400

    feat: bind ordinary semistability to quadratic support
    Closes #380.

--- DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Support.lean
+++ DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Support.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Support.Quotient
+import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Support.Semistable
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Support.ZeroChargeLattice
 
 /-! # Support properties -/
