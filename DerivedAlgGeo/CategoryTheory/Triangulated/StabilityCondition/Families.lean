776086fda6d723db3dcecf7db300da8f2458606a
Chris Dare <chris.dare@nalej.com>
2026-08-15 02:34:46 -0400

    feat: bind uniform support to varying fiber categories
    Closes #389.

--- DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Families.lean
+++ DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Families.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Ordinary
+import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.FiberwiseSupport
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Theorem22
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Weak
 
