ed569d8804b309a49387dbcb5c0bf894f7d9a259
Chris Dare <chris.dare@nalej.com>
2026-08-16 18:26:39 -0400

    Add SF7 mapping telescope foundation

--- DerivedAlgGeo/CategoryTheory/Triangulated/CompactlyGenerated/Existence.lean
+++ DerivedAlgGeo/CategoryTheory/Triangulated/CompactlyGenerated/Existence.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
 import DerivedAlgGeo.CategoryTheory.Triangulated.CompactlyGenerated
+import DerivedAlgGeo.CategoryTheory.Triangulated.CompactlyGenerated.Telescope
 import DerivedAlgGeo.CategoryTheory.Triangulated.TStructure.Aisle
 
 /-!
