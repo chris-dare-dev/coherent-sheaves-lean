dd65cd5ffe617b9690e40e7158845c9835de8431
Chris Dare <chris.dare@nalej.com>
2026-08-17 18:21:02 -0400

    feat: preserve affine K-projective pullback locus

--- DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Families.lean
+++ DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Families.lean
@@ -23,6 +23,7 @@ import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Ker
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Dqc
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Dqc.Affine
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Dqc.AffineKProjectivePullback
+import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.Dqc.AffineKProjectiveCoherence
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.GeometricBaseChange
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.FiniteTypeGeometry
 import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Families.ExactPullback
