409ce81ff8cf6188d713a30ff5f61c4f5698dce9
Chris Dare <chris.dare@nalej.com>
2026-08-17 01:26:05 -0400

    Build SF9 algebraic stack foundations

--- DerivedAlgGeo/AlgebraicGeometry/Stacks.lean
+++ DerivedAlgGeo/AlgebraicGeometry/Stacks.lean
@@ -4,5 +4,6 @@ Released under the MIT license.
 -/
 import DerivedAlgGeo.AlgebraicGeometry.Stacks.Basic
 import DerivedAlgGeo.AlgebraicGeometry.Stacks.Representable
+import DerivedAlgGeo.AlgebraicGeometry.Stacks.Algebraic
 
 /-! # Stacks in algebraic geometry -/
