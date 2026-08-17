4a761f1419a15942bf8db3f06560b4b09547e3b6
Chris Dare <chris.dare@nalej.com>
2026-08-16 21:08:07 -0400

    feat(sf7): construct Brown approximation tower

--- DerivedAlgGeo/CategoryTheory/Triangulated/CompactlyGenerated/IndExtension.lean
+++ DerivedAlgGeo/CategoryTheory/Triangulated/CompactlyGenerated/IndExtension.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
-import DerivedAlgGeo.CategoryTheory.Triangulated.CompactlyGenerated.Existence
+import DerivedAlgGeo.CategoryTheory.Triangulated.CompactlyGenerated.Brown
 import DerivedAlgGeo.CategoryTheory.Triangulated.TStructure.Restriction
 
 /-!
